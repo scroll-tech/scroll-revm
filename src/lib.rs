259023401a6cc803a807b3e7266b6cbe490fd805
Gregory Edison <gregory.edison1993@gmail.com>
2025-02-27 12:36:20 +0100

    style: update lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
-use l1block::L1BlockInfo;
-
 extern crate alloc;
 
+pub mod builder;
+
+pub use evm::ScrollEvm;
 pub mod evm;
+
+mod exec;
+
 pub mod handler;
+
 pub mod instructions;
+
 pub mod l1block;
+
 // TODO(greg): remove once revm exposes the pop macros.
 mod macros;
+
 pub mod precompile;
-mod spec;
+
 pub use spec::*;
+mod spec;
+
+pub use transaction::ScrollTransaction;
 mod transaction;
-pub use transaction::ScrollTx;
